673a8078d147aa6299ddb46a226b701055610732
Melany Willis <melexiw@gmail.com>
2023-08-12 21:04:42 -0500

    I forgot to commit

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,18 +1,31 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const [ boardName, setBoardName] = useState("No boards yet!");
+   const boards = [
+      { label: "Soup Recipes", value: "soups"}, 
+      { label: "Chicken Recipes", value: "chicken"}, 
+      { label: "Slow Cooker", value: "crockpot"}
+   ];
+
+   let boardOptions = boards.map((board, index) => {
+      return <option key={index} value={board.value}>{board.label}</option>;
+   });
 
    const handleChange = (event) => {
+      setBoardName(event.target.value)
    }
 
    return (
-      <div style={{paddingTop: "50px"}}>
-      <label>Save to Board: </label>
-      <select value={boardName} onChange={handleChange}>
-      </select>
+      <div>
+         <div style={{paddingTop: "50px"}}>
+         <label>Save to Board: </label>
+         <select value={boardName} onChange={handleChange}>
+            {boardOptions}
+         </select>
 
-      <p>Saved to {boardName}!</p>
+         <p>Saved to {boardName}!</p>
+         </div>
       </div>
    );
 }
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,11 +1,11 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://easyweeknightrecipes.com";
+   let authorPhoto = "https://easyweeknightrecipes.com/wp-content/uploads/2020/01/katerina.jpg";
+   let authorName = "Katerina";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} width="200px" />
          <div>
             <h3>{authorName}</h3>
             <a href={authorLink}></a> 
@@ -15,7 +15,7 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["chicken breasts", "chicken broth", "bacon", "cream cheese", "shredded cheddar cheese"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -34,8 +34,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Slow Cooker Crack Chicken Soup</h1>
+            <p>Slow Cooker Crack Chicken Soup is filled with tender shredded white meat chicken, real crumbles of crispy bacon, cream cheese, cheddar cheese, homemade ranch seasoning, and kale. It’s cozy comfort in a bowl!</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +47,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://easyweeknightrecipes.com/wp-content/uploads/2021/03/slow-cooker-crack-chicken-soup-9.jpg" alt="Slow Cooker Crack Chicken Soup" className="imageUpdates" height="450px"/>
    );
 }
 
